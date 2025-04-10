7da6fbfff630333edfe5105331c01149f5695bd5
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-04-10 20:48:20 +0700

    crud with nestjs module

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PostsService {
+  getPosts() {
+    return 'all posts';
+  }
+
+  createPost(body: any) {
+    return body;
+  }
+
+  getPostsById(id: string) {
+    return id;
+  }
+
+  updatePost(id: string, body: any) {
+    return { id, ...body };
+  }
+
+  deletePost(id: string) {
+    return `${id} deleted`;
+  }
+}

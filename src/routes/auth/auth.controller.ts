fb5b62b90199c30e60e74772792ab8b9ba902095
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-04-22 21:27:30 +0700

    validation with DTO

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { RegisterBodyDTO } from './auth.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() body) {
+  async register(@Body() body: RegisterBodyDTO) {
     const newUser = await this.authService.register(body)
     return newUser
   }

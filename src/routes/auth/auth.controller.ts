e4b27bcbab86dfdf1e2753a49adcacf7d88fd376
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-05-04 02:37:40 +0700

    serialization response

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, SerializeOptions } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import { RegisterBodyDTO } from './auth.dto'
+import { RegisterBodyDTO, RegisterResDTO } from './auth.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @SerializeOptions({ type: RegisterResDTO })
   @Post('register')
-  async register(@Body() body: RegisterBodyDTO) {
-    const newUser = await this.authService.register(body)
+  register(@Body() body: RegisterBodyDTO) {
+    const newUser = this.authService.register(body)
     return newUser
   }
 }
--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,15 +1,30 @@
-import { IsString } from "class-validator"
+import { Exclude } from 'class-transformer'
+import { IsString } from 'class-validator'
 
 export class LoginBodyDTO {
-    @IsString()
-    email: string
-    @IsString()
-    password: string
+  @IsString()
+  email: string
+  @IsString()
+  password: string
 }
 
 export class RegisterBodyDTO extends LoginBodyDTO {
-    @IsString()
-    name: string
-    @IsString()
-    confirmPassword: string
+  @IsString({ message: 'Name is string' })
+  name: string
+  @IsString()
+  confirmPassword: string
+}
+
+export class RegisterResDTO {
+  id: number
+  name: string
+  email: string
+  @Exclude()
+  password: string
+  createdAt: Date
+  updatedAt: Date
+
+  constructor(partial: Partial<RegisterResDTO>) {
+    Object.assign(this, partial)
+  }
 }
